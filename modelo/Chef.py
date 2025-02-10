3f1cc03939aca4d638ba92ccb0199268f37ffa03
JuanCamilo268 <juleonba@unal.edu.co>
2025-02-09 22:30:35 -0500

    Create Chef.py

---
+++ modelo/Chef.py
@@ -0,0 +1,37 @@
+from Empleado import Empleado
+
+class Chef(Empleado):
+    def __init__(self, id, nombre, direccion, edad, sucursal, antiguedad = 1, fecha = "28/01/2025", sueldo= 1500000):
+        super().__init__(id, nombre, direccion, edad, sueldo, "CHEF")
+        self.antiguedad = antiguedad
+        self.fechaDeContratacion = fecha
+        self.SUCURSAL = sucursal
+        self.ultimaCalificacion = 0
+        self.pedidosActuales = 0
+        self.puntaje = 0
+        self.disponible = True
+        self.proximoObjetivo = 0
+    
+    def getPuntaje(self): return self.puntaje
+
+    def getAntiguedad(self): return self.antiguedad
+
+    def getSucursal(self): return self.SUCURSAL
+
+    def getCalificacion(self): return self.ultimaCalificacion
+
+    def getPedidosActuales(self): return self.pedidosActuales
+
+    def isDisponible(self): return self.disponible
+
+    def getProximoObjetivo(self): return self.proximoObjetivo
+
+    def setPuntaje(self, puntaje): self.puntaje = puntaje
+
+    def setAntiguedad(self, x): self.antiguedad = x
+
+    def setCalificacion(self, x): self.ultimaCalificacion = x
+
+    def setDisponible(self, x): self.disponible = x
+
+    def setProximoObjetivo(self, x): self.proximoObjetivo = x
