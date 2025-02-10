a63559cedec34fa0ad7feb177a7a7ed7502b0a26
JuanCamilo268 <juleonba@unal.edu.co>
2025-02-09 22:30:38 -0500

    Update Empleado.py

---
+++ modelo/Empleado.py
@@ -0,0 +1,61 @@
+from Nombre import Nombre
+from Apellido import Apellido
+from random import randint
+
+class Empleado():
+    empleados = []
+
+    def __init__(self, id, nombre, direccion, edad, sueldo, rol):
+        self.ID = id
+        self.NOMBRE = nombre
+        self.DIRECCION  = direccion
+        self.EDAD = edad
+        self.sueldo = sueldo
+        self.ROL = rol
+        Empleado.empleados.append(self)
+
+    def __str__(self):
+        return "Nombre: " + self.NOMBRE + "/id: " + str(self.ID)
+    
+    @staticmethod
+    def generarNombre():
+        x = randint(1, 70)
+        y = randint(1, 70)
+        nombre = Nombre.getNombre(x)
+        apellido = Apellido.getNombre(y)
+        return nombre + " " + apellido
+
+    @staticmethod
+    def generarDocumento():
+        bien = False
+        while bien == False:
+            x = randint(10000, 99999)
+            bien = Empleado.verificar(x)
+        return x
+    
+    @staticmethod
+    def verificar(num):
+        for i in Empleado.empleados:
+            if i.ID == num: return False
+        return True
+    
+    @staticmethod
+    def despedir(empleado):
+        Empleado.empleados.remove(empleado)
+
+    @staticmethod
+    def getPersonal(): return Empleado.empleados
+
+    def getId(self): return self.ID
+
+    def getNombre(self): return self.NOMBRE
+
+    def getDIreccion(self): return self.DIRECCION
+
+    def getEdad(self): return self.EDAD
+
+    def getSueldo(self): return self.sueldo
+
+    def getRol(self): return self.ROL
+
+    def setSueldo(self, nuevo): self.sueldo = nuevo
