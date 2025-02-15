004b0917144286e5fa5a3ab6af657faa27d6ee10
JuanCamilo268 <juleonba@unal.edu.co>
2025-02-14 19:27:05 -0500

    Update Plato.py

--- modelo/Plato.py
+++ modelo/Plato.py
@@ -4,3 +4,12 @@ class Plato:
         self.ID = id
         self.nombre = nombre
         self.precio = precio
+    
+    def getId(self): return self.ID
+
+    def getNombre(self): return self.nombre
+
+    def getPrecio(self): return self.precio
+
+    def __str__(self):
+        return self.nombre + ": $" + str(self.precio)
