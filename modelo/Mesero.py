b87cb70a854455f509f7009fc0ae8717c062bf2f
JuanCamilo268 <juleonba@unal.edu.co>
2025-02-18 11:49:42 -0500

    Update Mesero.py

--- modelo/Mesero.py
+++ modelo/Mesero.py
@@ -12,7 +12,38 @@ class Mesero(Empleado):
         self.pedidosActuales = 0
         self.puntaje = 0
         self.disponible = True
-        self.proximoObjetivo = 0
+        self.proximoObjetivo = 20
+    
+    def ganarPuntos(self, calificacion):
+        self.pedidosAtendidos += 1
+        if calificacion == 1:
+            if self.puntaje >= 2:
+                self.puntaje -= 2
+            else:
+                self.puntaje = 0
+        elif calificacion == 2:
+            if self.puntaje >= 1:
+                self.puntaje -= 1
+            else: 
+                self.puntaje = 0
+        elif calificacion == 3:
+            pass
+        elif calificacion == 4:
+            self.puntaje += 1
+        elif calificacion == 5:
+            self.puntaje += 2
+        else:
+            pass
+
+        if self.getSueldo() > 2300000:
+            self.puntaje = 0
+            self.setSueldo(2300000)
+        
+        if self.puntaje >= self.proximoObjetivo:
+            aumento = round(self.sueldo * 0.1)
+            self.sueldo += aumento
+            self.puntaje = 0
+            self.proximoObjetivo = 50
     
     def getPuntaje(self): return self.puntaje
 
